e1bc9ca862279f39c3038a68b7a7740e348d9619
arnavchevula <nagarjunchevula@Nagarjuns-MacBook-Pro-2.local>
2024-08-01 02:24:53 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,10 +17,60 @@ const data = [
   }
 ];
 
+const links = [
+  {
+    heading: "Company",
+    links: ["Home", "About", "Career"]
+  },
+  {
+    heading: "Articles",
+    links: ["Blog", "Journal", "RSS Feed"]
+  },
+  {
+    heading: "Resources",
+    links: ["Documentation", "Support", "Status"]
+  },
+  {
+    heading: "Contact",
+    links: ["GitHub", "Twitter", "Community"]
+  },
+  {
+    heading: "Legal",
+    links: [
+      "Security",
+      "Privacy Policy",
+      "Terms of Service",
+      "Acceptable Use Policy"
+    ]
+  }
+];
 const Footer = color => {
   return (
     <div className="Footer__Wrapper" style={{ background: color.color }}>
+      <div className="Footer__List">
+        {/* <span>Company</span>
+          <div className="Footer__Links"></div>
+          <span>About</span>
+          <span>Contact</span>
+          <span>Careers</span> */}
+
+        {links.map(element => {
+          return (
+            <div className="Footer__Section">
+              <h3>{element.heading}</h3>
+              <div className="Link__List">
+                {element.links.map(link => {
+                  return <a>{link}</a>;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <ul className="Social__List">
+        <div className="Copyright">
+          <p>2024 COPYRIGHT @ Binary Computing, Inc</p>
+        </div>
         {data.map((value, index) => {
           return (
             <li style={{ padding: "10px", margin: "10px" }}>
